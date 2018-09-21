390621342d5b30157f401746b475fd37e698dac2
M Ryan <mxryan@live.com>
2018-09-20 20:18:12 -0400

    updated style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,19 @@
+//to do: hide start button when game starts
+
 var timerDisplay = document.querySelector("#timer");
 var questionDisplay = document.querySelector("#question");
 var answerDisplay = document.querySelector("#answer-choices");
 var gameArea = document.querySelector("game-area");
+var breakLabelDisplay = document.querySelector("#break-label");
 var breakTimerDisplay = document.querySelector("#break-timer");
 var infoDisplay = document.querySelector("#info");
+var detailsDisplay = document.querySelector("#info-details");
 var quests = [];
 var curQ = -1; // index current question
 var questionAnswered = false;
 var gameStarted = false;
 var timeRemaining = 30;
-var breakTimeRemaining = 4;
+var breakTimeRemaining = 3;
 var timerID = null;
 var breakTimerID = null;
 var results = {
@@ -18,14 +22,13 @@ var results = {
   unanswered: 0
 }
 
-
 function displayNewQA() {
-  console.log("displayNewQA()");
   curQ++;
   breakTimerDisplay.textContent = "Round Active!"
   infoDisplay.textContent = "";
+  detailsDisplay.textContent = "";
+  breakLabelDisplay.textContent ="";
   if (!gameStarted) {
-    console.log("displayNewQA() entif !gameStarted");
     gameStarted = true;
     grabNewQuests();
     results = {
@@ -35,7 +38,6 @@ function displayNewQA() {
     };
   }
   if (curQ < quests.length) {
-    console.log("displayNewQA() entif curQ < quests.length");
     questionDisplay.innerHTML = quests[curQ].question;
     questionAnswered = false;
     setAnswerChoices();
@@ -76,7 +78,6 @@ function grabNewQuests() {
 
 
 function setAnswerChoices() {
-  console.log("setAnswerChoices())");
   while (answerDisplay.firstChild) {
     answerDisplay.removeChild(answerDisplay.firstChild);
   }
@@ -86,7 +87,6 @@ function setAnswerChoices() {
     choice.value = i;
     choice.innerHTML = quests[curQ].answers[i];
     choice.addEventListener("click", (e) => {
-
       handleAnswer(e.target.value);
     })
     answerDisplay.appendChild(choice);
@@ -94,7 +94,6 @@ function setAnswerChoices() {
 }
 
 function setCountdown() {
-  console.log("setCountdown())");
   if (timerID) clearInterval(timerID);
   timeRemaining = 30;
   timerDisplay.textContent = timeRemaining;
@@ -102,32 +101,32 @@ function setCountdown() {
 }
 
 function handleAnswer(answerVal) {
-  console.log("handleAnswer()")
   if (!questionAnswered) {
-    console.log("handleAnswer entif !questionAnswered")
+    breakTimerDisplay.textContent = 3
     questionAnswered = true;
+    breakLabelDisplay.textContent ="Break remaining:";
     if (answerVal == quests[curQ].indexCorrect) {
       results.correct++;
       infoDisplay.textContent = "Correct!!";
+      detailsDisplay.textContent = "You did it!";
     } else {
       results.incorrect++;
       var correctAnswer = quests[curQ].answers[quests[curQ].indexCorrect];
-      infoDisplay.textContent = "Wrong!!" + "Correct answer was: " + correctAnswer;
+      infoDisplay.textContent = "Wrong!!";
+      detailsDisplay.textContent = "Correct answer was: " + correctAnswer;
     }
     startBreak();
   }
 }
 
 function startBreak() {
-  console.log("startBreak()");
   clearInterval(timerID);
   timerID = null;
-  breakTimeRemaining = 4;
+  breakTimeRemaining = 3;
   breakTimerID = setInterval(breakTick, 1000);
 }
 
 function tick() {
-  console.log("tick()");
   timeRemaining--;
   timerDisplay.textContent = timeRemaining;
   if (timeRemaining === 0) {
@@ -141,7 +140,6 @@ function tick() {
 }
 
 function breakTick() {
-  console.log("breakTick()");
   breakTimeRemaining--;
   breakTimerDisplay.textContent = breakTimeRemaining;
   if (breakTimeRemaining === 0 && curQ === quests.length - 1) {
@@ -149,14 +147,12 @@ function breakTick() {
     gameOver();
     breakTimerDisplay.textContent = "Game Over!"
   } else if (breakTimeRemaining === 0 && curQ < quests.length - 1) {
-    
     clearInterval(breakTimerID);
     displayNewQA();
   }
 }
 
 function gameOver() {
-  console.log("gameOver");
   if (timerID) clearInterval(timerID);
   curQ = -1;
   gameStarted = false;
@@ -164,7 +160,6 @@ function gameOver() {
 }
 
 function displayResults() {
-  console.log("displayResults");
   timerDisplay.textContent = "Game over!";
   questionDisplay.textContent = "How did you do?";
   while (answerDisplay.firstChild) {
@@ -176,7 +171,6 @@ function displayResults() {
     "</p>";
   answerDisplay.innerHTML = resultsHTML;
 }
-
 
 document.querySelector("#start-game").addEventListener("click", () => {
   if (!gameStarted) displayNewQA();
